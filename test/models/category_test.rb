eabed9896bdb3fa0135c0d6a428b3f370247b65d
Artur M <amrozinski@gmail.com>
2020-07-04 17:39:47 +0100

    added funtional test and created new category creation item

---
+++ test/models/category_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  def setup
+    @category = Category.new(name: "Sports")
+  end
+
+  test "category should be valid" do
+    assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
+  test  "name should not be too long" do
+    @category.name = "Aa" * 13
+    assert_not @category.valid?
+
+  end
+
+  test "name should not be too short" do
+    @category.name = "Aa" * 1
+    assert_not @category.valid?
+  end
+
+end
